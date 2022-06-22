cf0b5a6f152188714cc622a46e97297b725a41af
Cervus23 <billiatrian23@gmail.com>
2022-06-22 19:02:09 +0300

    Refactored some code

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -3,122 +3,105 @@ import { Currency } from "../../types/currency";
 // import style from "./style.module.scss";
 
 const Main = (props: { rate: Currency[] }) => {
-  const input = useRef(null);
-  const output = useRef(null);
+  const uah = { ccy: "UAH", base_ccy: "UAH", buy: "1", sale: "1" };
 
-  const [inputValue, setInputValue] = useState<number>();
-  const [outputValue, setOutputValue] = useState<number>();
+  const input = useRef<HTMLInputElement>(null);
+  const output = useRef<HTMLInputElement>(null);
 
-  // const [inputSelect, setInputSelect] = useState("");
-  // const [outputSelect, setOutputSelect] = useState("");
+  const inputSelect = useRef<HTMLSelectElement>(null);
+  const outputSelect = useRef<HTMLSelectElement>(null);
 
-  const [inputCurr, setInputCurr] = useState<Currency>({
-    ccy: "",
-    base_ccy: "",
-    buy: "",
-    sale: "",
-  });
-  const [outputCurr, setOutputCurr] = useState<Currency>({
-    ccy: "",
-    base_ccy: "",
-    buy: "",
-    sale: "",
-  });
+  const [inputCurr, setInputCurr] = useState<Currency>(uah);
+  const [outputCurr, setOutputCurr] = useState<Currency>(uah);
 
-  // useEffect(() => {
-  //   console.log(inputCurr);
-  //   console.log(outputCurr);
-  // }, [inputCurr, outputCurr]);
+  const convert = (value: number, direction: "direct" | "reverse") => {
+    const convertFrom = direction === "direct" ? inputCurr : outputCurr;
+    const convertTo = direction === "direct" ? outputCurr : inputCurr;
 
-  const OnInputChangeHanler = (e) => {
-    if (inputCurr.ccy === "UAH" && outputCurr.ccy !== "UAH") {
-      const result = +parseFloat(
-        (e.target.valueAsNumber / parseFloat(outputCurr.sale)).toFixed(2)
-      );
-      output.current.value = result;
+    let result = 0;
+
+    // Conversion from UAH to other currency
+    if (convertFrom.ccy === "UAH" && convertTo.ccy !== "UAH") {
+      result = +parseFloat((value / parseFloat(convertTo.sale)).toFixed(2));
     }
-    if (inputCurr.ccy !== "UAH" && outputCurr.ccy === "UAH") {
-      const result = +parseFloat(
-        (e.target.valueAsNumber * parseFloat(inputCurr.buy)).toFixed(2)
-      );
-      output.current.value = result;
-    } else {
-      const result = +parseFloat(
+    // Conversion from other currency to UAH
+    if (convertFrom.ccy !== "UAH" && convertTo.ccy === "UAH") {
+      result = +parseFloat((value * parseFloat(convertFrom.buy)).toFixed(2));
+    }
+    // Conversion between other currencies (There is double-conversion because the base currency is UAH)
+    else {
+      result = +parseFloat(
         (
-          (e.target.valueAsNumber * parseFloat(inputCurr.buy)) /
-          parseFloat(outputCurr.sale)
+          (value * parseFloat(convertFrom.buy)) /
+          parseFloat(convertTo.sale)
         ).toFixed(2)
       );
-      output.current.value = result;
+    }
+
+    if (direction === "direct") {
+      output.current!.value = `${result}`;
+    } else {
+      input.current!.value = `${result}`;
     }
   };
 
-  const OnOutputChangeHanler = (e) => {
-    if (outputCurr.ccy === "UAH" && inputCurr.ccy !== "UAH") {
-      const result = +parseFloat(
-        (e.target.valueAsNumber / parseFloat(inputCurr.sale)).toFixed(2)
-      );
-      input.current.value = result;
-    }
-    if (outputCurr.ccy !== "UAH" && inputCurr.ccy === "UAH") {
-      const result = +parseFloat(
-        (e.target.valueAsNumber * parseFloat(outputCurr.buy)).toFixed(2)
-      );
-      input.current.value = result;
-    } else {
-      const result = +parseFloat(
-        (
-          (e.target.valueAsNumber * parseFloat(outputCurr.buy)) /
-          parseFloat(inputCurr.sale)
-        ).toFixed(2)
-      );
-      input.current.value = result;
-    }
-  };
+  const selectionHandler = (value: string, selectType: "input" | "output") => {
+    switch (true) {
+      case selectType === "input" && value === outputSelect.current!.value:
+        setOutputCurr(uah);
 
-  const OnInputSelectHandler = (e) => {
-    switch (e.target.value) {
-      case "UAH":
-        setInputCurr({ ccy: "UAH", base_ccy: "UAH", buy: "1", sale: "1" });
+        outputSelect.current!.value = "UAH";
         break;
-      case "":
-        setInputCurr({ ccy: "", base_ccy: "", buy: "", sale: "" });
+
+      case selectType === "output" && value === inputSelect.current!.value:
+        setInputCurr(uah);
+
+        inputSelect.current!.value = "UAH";
         break;
-      default:
+
+      case selectType === "input" &&
+        value !== outputSelect.current!.value &&
+        value === "UAH":
+        setInputCurr(uah);
+        break;
+
+      case selectType === "output" &&
+        value !== inputSelect.current!.value &&
+        value === "UAH":
+        setOutputCurr(uah);
+        break;
+
+      case selectType === "input" && value !== outputSelect.current!.value:
         setInputCurr(
-          props.rate.filter((currency) => currency.ccy === e.target.value)[0]
+          props.rate.filter((currency) => currency.ccy === value)[0]
         );
         break;
-    }
-  };
 
-  const OnOutputSelectHandler = (e) => {
-    switch (e.target.value) {
-      case "UAH":
-        setOutputCurr({ ccy: "UAH", base_ccy: "UAH", buy: "1", sale: "1" });
-        break;
-      case "":
-        setOutputCurr({ ccy: "", base_ccy: "", buy: "", sale: "" });
-        break;
-      default:
+      case selectType === "output" && value !== inputSelect.current!.value:
         setOutputCurr(
-          props.rate.filter((currency) => currency.ccy === e.target.value)[0]
+          props.rate.filter((currency) => currency.ccy === value)[0]
         );
+        break;
+
+      default:
         break;
     }
   };
 
   return (
-    <div>
+    <form>
       <div>
         <input
-          placeholder="enter a value"
           type="number"
           ref={input}
-          onChange={OnInputChangeHanler}
+          onChange={(e) => convert(e.target.valueAsNumber, "direct")}
+          defaultValue={0}
         />
-        <select onChange={OnInputSelectHandler}>
-          <option value="">Choose a currency type</option>
+        <select
+          onChange={(e) => selectionHandler(e.target.value, "input")}
+          ref={inputSelect}
+        >
+          {/* <option value="">Choose a currency type</option> */}
           <option value="UAH">UAH</option>
           {props.rate.map((currency) => (
             <option value={currency.ccy} key={currency.ccy}>
@@ -129,13 +112,15 @@ const Main = (props: { rate: Currency[] }) => {
       </div>
       <div>
         <input
-          placeholder="enter a value"
           type="number"
           ref={output}
-          onChange={OnOutputChangeHanler}
+          onChange={(e) => convert(e.target.valueAsNumber, "reverse")}
+          defaultValue={0}
         />
-        <select onChange={OnOutputSelectHandler}>
-          <option value="">Choose a currency type</option>
+        <select
+          onChange={(e) => selectionHandler(e.target.value, "output")}
+          ref={outputSelect}
+        >
           <option value="UAH">UAH</option>
           {props.rate.map((currency) => (
             <option value={currency.ccy} key={currency.ccy}>
@@ -144,7 +129,7 @@ const Main = (props: { rate: Currency[] }) => {
           ))}
         </select>
       </div>
-    </div>
+    </form>
   );
 };
 
