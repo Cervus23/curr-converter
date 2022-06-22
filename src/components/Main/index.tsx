4686baebbbb6ac1209ccd52078b2da5dfcf55b7a
Cervus23 <billiatrian23@gmail.com>
2022-06-22 16:39:07 +0300

    Added currency converter

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { Currency } from "../../types/currency";
 // import style from "./style.module.scss";
 
@@ -6,14 +6,118 @@ const Main = (props: { rate: Currency[] }) => {
   const input = useRef(null);
   const output = useRef(null);
 
-  const [inputCurr, setInputCurr] = useState("");
-  const [outputCurr, setOutputCurr] = useState("");
+  const [inputValue, setInputValue] = useState<number>();
+  const [outputValue, setOutputValue] = useState<number>();
+
+  // const [inputSelect, setInputSelect] = useState("");
+  // const [outputSelect, setOutputSelect] = useState("");
+
+  const [inputCurr, setInputCurr] = useState<Currency>({
+    ccy: "",
+    base_ccy: "",
+    buy: "",
+    sale: "",
+  });
+  const [outputCurr, setOutputCurr] = useState<Currency>({
+    ccy: "",
+    base_ccy: "",
+    buy: "",
+    sale: "",
+  });
+
+  // useEffect(() => {
+  //   console.log(inputCurr);
+  //   console.log(outputCurr);
+  // }, [inputCurr, outputCurr]);
+
+  const OnInputChangeHanler = (e) => {
+    if (inputCurr.ccy === "UAH" && outputCurr.ccy !== "UAH") {
+      const result = +parseFloat(
+        (e.target.valueAsNumber / parseFloat(outputCurr.sale)).toFixed(2)
+      );
+      output.current.value = result;
+    }
+    if (inputCurr.ccy !== "UAH" && outputCurr.ccy === "UAH") {
+      const result = +parseFloat(
+        (e.target.valueAsNumber * parseFloat(inputCurr.buy)).toFixed(2)
+      );
+      output.current.value = result;
+    } else {
+      const result = +parseFloat(
+        (
+          (e.target.valueAsNumber * parseFloat(inputCurr.buy)) /
+          parseFloat(outputCurr.sale)
+        ).toFixed(2)
+      );
+      output.current.value = result;
+    }
+  };
+
+  const OnOutputChangeHanler = (e) => {
+    if (outputCurr.ccy === "UAH" && inputCurr.ccy !== "UAH") {
+      const result = +parseFloat(
+        (e.target.valueAsNumber / parseFloat(inputCurr.sale)).toFixed(2)
+      );
+      input.current.value = result;
+    }
+    if (outputCurr.ccy !== "UAH" && inputCurr.ccy === "UAH") {
+      const result = +parseFloat(
+        (e.target.valueAsNumber * parseFloat(outputCurr.buy)).toFixed(2)
+      );
+      input.current.value = result;
+    } else {
+      const result = +parseFloat(
+        (
+          (e.target.valueAsNumber * parseFloat(outputCurr.buy)) /
+          parseFloat(inputCurr.sale)
+        ).toFixed(2)
+      );
+      input.current.value = result;
+    }
+  };
+
+  const OnInputSelectHandler = (e) => {
+    switch (e.target.value) {
+      case "UAH":
+        setInputCurr({ ccy: "UAH", base_ccy: "UAH", buy: "1", sale: "1" });
+        break;
+      case "":
+        setInputCurr({ ccy: "", base_ccy: "", buy: "", sale: "" });
+        break;
+      default:
+        setInputCurr(
+          props.rate.filter((currency) => currency.ccy === e.target.value)[0]
+        );
+        break;
+    }
+  };
+
+  const OnOutputSelectHandler = (e) => {
+    switch (e.target.value) {
+      case "UAH":
+        setOutputCurr({ ccy: "UAH", base_ccy: "UAH", buy: "1", sale: "1" });
+        break;
+      case "":
+        setOutputCurr({ ccy: "", base_ccy: "", buy: "", sale: "" });
+        break;
+      default:
+        setOutputCurr(
+          props.rate.filter((currency) => currency.ccy === e.target.value)[0]
+        );
+        break;
+    }
+  };
 
   return (
     <div>
       <div>
-        <input placeholder="enter a value" type="number" ref={input}></input>
-        <select onChange={(e) => setInputCurr(e.target.value)}>
+        <input
+          placeholder="enter a value"
+          type="number"
+          ref={input}
+          onChange={OnInputChangeHanler}
+        />
+        <select onChange={OnInputSelectHandler}>
           <option value="">Choose a currency type</option>
           <option value="UAH">UAH</option>
           {props.rate.map((currency) => (
@@ -24,8 +128,13 @@ const Main = (props: { rate: Currency[] }) => {
         </select>
       </div>
       <div>
-        <input placeholder="enter a value" type="number" ref={output}></input>
-        <select onChange={(e) => setOutputCurr(e.target.value)}>
+        <input
+          placeholder="enter a value"
+          type="number"
+          ref={output}
+          onChange={OnOutputChangeHanler}
+        />
+        <select onChange={OnOutputSelectHandler}>
           <option value="">Choose a currency type</option>
           <option value="UAH">UAH</option>
           {props.rate.map((currency) => (
