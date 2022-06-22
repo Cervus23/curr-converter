67786a0646520c163d968535cf71303d8b65c364
Cervus23 <billiatrian23@gmail.com>
2022-06-22 20:57:42 +0300

    Fixing some details

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// @ts-ignore
 import App from "./App.tsx";
-
 import "./index.module.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
